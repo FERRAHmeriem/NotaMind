2e70d489ff88c2b91600967f9a7f9aa01d594d55
Meriem FERRAH <149694634+FERRAHmeriem@users.noreply.github.com>
2025-08-17 17:02:44 +0100

    Update extensions.js

--- app/workplace/component/extensions.js
+++ app/workplace/component/extensions.js
@@ -193,9 +193,10 @@ function Extensions({ editor }) {
                 )}
               </button>
               {!loading && (
-                <span className=" absolute -bottom-8  -translate-x-1/2 scale-0 group-hover:scale-100 transition-all bg-purple-400 text-stone-800 text-xs px-2 py-1 rounded shadow">
+                <span className="absolute -bottom-8 -translate-x-1/2 scale-0 group-hover:scale-100 transition-all bg-purple-400 text-stone-800 text-xs px-2 py-1 rounded shadow">
                   Use AI ✨
                 </span>
+
               )}
             </div>
           </div>
